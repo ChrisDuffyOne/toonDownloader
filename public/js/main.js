2da8e9a48ad31ce153d3edb2be1b50629fc3806a
Chris Duffy <chrisduffyone@gmail.com>
2016-10-19 15:21:22 +0000

    First Commit

---
+++ public/js/main.js
@@ -0,0 +1,209 @@
+console.log('ToonIs Main: Online');
+var socket = io();
+var socketDownloadId; 
+
+var VideoList = function() {
+    this.downloadIndex = 0; //DEBUG
+    this.videos = [];
+    this.episodes = [];
+    this.downloadList = [];
+    this.mp4Urls = [];
+    this.getVideoList();
+    this.socketEvents();
+};
+
+//-----------Get TV Series List-----------//
+VideoList.prototype.getVideoList = function(){
+    var ajax = $.ajax('/listShow', {
+        type: 'GET',
+        dataType: 'json'
+    });
+    ajax.done(this.getVideoListDone.bind(this));
+};
+
+VideoList.prototype.getVideoListDone = function(videos) {
+    this.videos = videos;
+    
+    var displayVideos = [];
+    $.each(videos, function(i, video) {
+        displayVideos.push('<li><button type="button" class="tvShowButton" data="'+video.url+'">'+video.name+'</button></li>');
+    });
+    $('#videoList').append(displayVideos.join(''));
+    
+    //Get Episode List
+    var videoList = this;
+    $('#videoList li button').click(function(){
+        var urlData = $(this).attr('data');
+        videoList.getEpisodeList(urlData);
+    });
+};
+
+//-----------Get Episodes List-----------//
+VideoList.prototype.getEpisodeList = function(urlData){
+    var url = {'urlData': urlData};
+    var ajax = $.ajax('/listEpisode', {
+        type: 'POST',
+        data: JSON.stringify(url),
+        dataType: 'json',
+        contentType: 'application/json'
+    });
+    ajax.done(this.getEpisodeListDone.bind(this));
+};
+
+VideoList.prototype.getEpisodeListDone = function(episodes){
+
+    this.episodes = [];
+     $('#episodeList').empty();
+    
+
+    this.episodes = episodes;
+    episodes.reverse();
+    
+    var displayEpisodes = [];
+    $.each(episodes, function(i, ep) {
+        displayEpisodes.push('<li><button type="button" class="tvEpisodeButton" data="'+ep.url+'">'+ep.name+'</button></li>');
+    });
+    $('#episodeList').append(displayEpisodes.join(''));
+    
+    //Add episode to download cue
+    var videoList = this;
+    $('#episodeList li button').click(function(){
+        var urlData = $(this).attr('data');
+        var urlName = $(this).html();
+        var episodeID = videoList.genEpisodeId(); //DEBUG
+        
+        //check if episode is a duplicate
+        if(videoList.rmDupEpisode(urlName)){ console.log('DUPLICATE ENTRY!: REMOVED');}
+        else{ videoList.downloadList.push({name: urlName, url: urlData, fileUrl: undefined, fileID: episodeID});}
+        
+        //fire off ajax request for mp4 url
+        $(this).toggleClass("selectedEpisode");
+        if($(this).hasClass("selectedEpisode")){
+            var getUrl = {'getUrl': urlData};
+            videoList.getFileUrl(getUrl);
+        };
+    });
+};
+
+//-----------get mp4 url and assign to download slot----------//
+VideoList.prototype.getFileUrl = function(getUrl){
+    var ajax = $.ajax('/getMp4Url', {
+        type: 'POST',
+        data: JSON.stringify(getUrl),
+        dataType: 'json',
+        contentType: 'application/json'
+    });
+    ajax.done(this.getFileUrlDone.bind(this));
+}
+
+VideoList.prototype.getFileUrlDone = function(fileUrl){
+    for(var i=0; i<this.downloadList.length; i++){
+        if(this.downloadList[i].url == fileUrl.refHTML){
+            this.downloadList[i].fileUrl = fileUrl.filePath;
+        }
+    }
+};
+
+//-----------Helper Functions-----------//
+VideoList.prototype.rmDupEpisode = function(episodeName){
+    for(var i=0; i<this.downloadList.length; i++){
+        if(this.downloadList[i].name === episodeName){
+            this.downloadList.splice(i, 1);
+            return true;
+        }
+    };
+};
+
+
+//-----------DEBUG Socket.io events-----------//
+VideoList.prototype.socketEvents = function(){
+    var videoList = this;
+    
+    //Incoming Events
+    socket.on('clientID', function(clientId){
+            socketDownloadId = clientId;
+    });
+    socket.on('requestNext', function(){
+            videoList.downloadIndex++;
+            if(videoList.downloadIndex === videoList.downloadList.length){
+                console.log('All Files Downloaded');
+            }else{
+                var testAutoClick = "#" + videoList.downloadList[videoList.downloadIndex].fileID;
+                $(testAutoClick).get(0).click();
+                console.log('NEXT DOWNLOAD CALL!');
+            }
+    });
+    //DEBUG: Untested
+    socket.on('requestRetry', function(){
+            if(videoList.downloadIndex === videoList.downloadList.length){
+                console.log('All Files Downloaded');
+            }else{
+                var testAutoClick = "#" + videoList.downloadList[videoList.downloadIndex].fileID;
+                $(testAutoClick).get(0).click();
+                console.log('NEXT DOWNLOAD CALL!');
+            }
+    });
+    
+    //Outgoing Events
+    $('#downloadBatch').click(function(){
+        
+        console.log(videoList.downloadList); //DEBUG
+        
+        //generate a list of link downloads
+        var downloadBatchList = [];
+        for(var i=0; i<videoList.downloadList.length; i++){
+            var fileUrl = videoList.downloadList[i].fileUrl;
+            var fileID = videoList.downloadList[i].fileID;
+            var fileName = videoList.downloadList[i].name;
+            //Remove first two chars
+            var urlSocketID = socketDownloadId.slice(2, (socketDownloadId.length));
+            
+            downloadBatchList.push('<li><a href="/downloadEpisode/'+urlSocketID+'/'+fileID+'/" id="'+fileID+'" download="'+fileName+'">Download'+i+'</a></li>'); //DEBUG
+        }
+        $('#downloadBatchHidden').append(downloadBatchList.join(''));
+        socket.emit('downloadBatch', videoList.downloadList);
+        
+        //DEBUG Start auto download
+        socket.on('downloadReady', function(message){
+            var testAutoClick = "#" + videoList.downloadList[videoList.downloadIndex].fileID;
+            $(testAutoClick).get(0).click();
+        });
+
+    });
+};
+
+//-----------Generate episode id-----------//
+VideoList.prototype.genEpisodeId = function(){
+    //generate random hex
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    for(var i=0; i < 8; i++){
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    
+    //make sure it isn't a duplicate
+    for(var i=0; i < this.downloadList.length; i++){
+        if(text === this.downloadList[i].fileID){
+            return this.genEpisodeId();
+        }
+    }
+    return text;
+}
+
+function toonDownloader(){
+    var contentList = new VideoList();
+}
+
+$(document).ready(function(){
+    toonDownloader();
+    
+
+    //AUTO DOWNLOAD THE LIST OF VIDEOS YOU WANT//
+    
+    //$('#dlTest').get(0).click(); //Automatically download Spoof click Works!
+    
+    /*for(var i=0; i<2; i++){
+        $('#dlTest').get(0).click();
+    }*/ //Also works for multiple downloads
+});
+
---
+++ server.js
@@ -0,0 +1,171 @@
+var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var fs = require('fs');
+
+var path = require('path');
+var mime = require('mime');
+
+//Web Scraping
+var request = require('request');
+var cheerio = require('cheerio');
+
+//Home Static
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+//DEBUG Socket.io
+var toonDownloadList = []; //DEBUG
+var socket_io = require('socket.io'); //DEBUG
+var server = http.Server(app);
+var io = socket_io(server);
+
+//Route: Get Series List
+app.get('/listShow', function(req, res){
+  console.log('listShow Endpoint');
+  var urls = [];
+  
+  request('http://www.toon.is', function(error, response, body){
+    if(!error && response.statusCode == 200){
+      var $ = cheerio.load(body);
+      $('li a', '#ul_categories').each(function(){
+        urls.push({name: $(this).html(), url: $(this).attr('href')});
+      });
+      res.status(200).json(urls);
+    }
+  });
+});
+
+//Route: Get Episode List
+app.post('/listEpisode', function(req, res){
+  var urls = [];
+  
+  request(req.body.urlData, function(error, response, body){
+    if(!error && response.statusCode == 200){
+      var $ = cheerio.load(body);
+      $('li a', '.videolist').each(function(){
+        
+        var episodeName = this.children[3].children[0].data;
+        var episodeLink = this.attribs.href;
+        urls.push({name: episodeName, url: episodeLink});
+      });
+      res.status(200).json(urls);
+    }
+  });
+  
+});
+
+//Route: Get Video File Url
+app.post('/getMp4Url', function(req, res){
+  
+  var scriptArray = [];
+  
+  request(req.body.getUrl, function(error, response, body){
+    if(!error && response.statusCode == 200){
+      var $ = cheerio.load(body);
+      $('script').each(function(){
+        scriptArray.push(this);
+      });
+      
+      var flashVarCopy = scriptArray[4].children[0].data;
+      
+      //Find start and end to file path
+      var fileStart = flashVarCopy.search('file: ')+ 7;
+      var fileEnd = flashVarCopy.search('.mp4') + 4;
+      var fileLength = fileEnd - fileStart;
+      
+      //Splice out the file path and remove backslashes
+      var urlExtract = flashVarCopy.substr(fileStart, fileLength);
+      var urlExtractEscaped = urlExtract.replace(/\\/g, '');
+       
+       var returnURLinfo = {filePath: urlExtractEscaped, refHTML: req.body.getUrl};
+       res.status(200).json(returnURLinfo);
+    }
+  });
+});
+
+//DEBUG
+io.on('connection', function(socket){
+  
+  //Send client their socket id
+  var downloadId = socket.id;
+  io.to(downloadId).emit('clientID', downloadId);
+  
+  socket.on('downloadBatch', function(downloadList) {
+        toonDownloadList.push({clientId: downloadId, clientList: downloadList});
+        io.to(downloadId).emit('downloadReady', 'Youre download is ready');
+  });
+  
+  //Route: Send Video Download to client
+  app.get('/downloadEpisode/:socketRef/:videoRef', function(req, res){
+  
+    var videoToDownload;
+    req.params.socketRef = "/#" + req.params.socketRef;
+    
+    
+    for(var i=0; i<toonDownloadList.length; i++){
+      if(toonDownloadList[i].clientId === req.params.socketRef){
+        
+        for(var k=0; k<toonDownloadList[i].clientList.length; k++){
+          if(toonDownloadList[i].clientList[k].fileID === req.params.videoRef){
+            videoToDownload = (toonDownloadList[i].clientList[k].fileUrl).replace(/\\/g, '');
+            console.log('Found Video ID');
+          }
+        }
+      }
+    }
+    
+    //Start Video Download:
+    request(videoToDownload).pipe(res); //DEBUG
+    //request('http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg').pipe(res); //DEBUG
+    
+    //DEBUG issue request for next video
+    req.on("close", function() { 
+      console.log('Connection: Closed Unexpectly');
+      io.to(req.params.socketRef).emit('requestRetry', 'Retry Download');
+    });
+    req.on("end", function() { 
+      console.log('Connection: Ended Normally'); 
+      io.to(req.params.socketRef).emit('requestNext', 'Next Download');
+    });
+  });
+  
+});
+
+//Route: Send Video Download to client
+/*app.get('/downloadEpisode/:socketRef/:videoRef', function(req, res){
+  
+  var videoToDownload;
+  req.params.socketRef = "/#" + req.params.socketRef;
+  
+  
+  for(var i=0; i<toonDownloadList.length; i++){
+    if(toonDownloadList[i].clientId === req.params.socketRef){
+      
+      for(var k=0; k<toonDownloadList[i].clientList.length; k++){
+        if(toonDownloadList[i].clientList[k].fileID === req.params.videoRef){
+          videoToDownload = (toonDownloadList[i].clientList[k].fileUrl).replace(/\\/g, '');
+          console.log('Found Video ID');
+        }
+      }
+    }
+  }
+  
+  //Start Video Download:
+  request(videoToDownload).pipe(res);
+  
+  //DEBUG issue request for next video
+  req.on("close", function() { 
+    console.log('Connection: Closed Unexpectly'); 
+  });
+  req.on("end", function() { 
+    console.log('Connection: Ended Normally'); 
+    
+  });
+});*/
+
+
+
+server.listen(process.env.PORT || 8080); 
+console.log('ToonIs Downloader: Online');
